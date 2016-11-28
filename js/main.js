324a842f3eb53a8aa4907155da65cce6bc99da3f
Lauren Yockey <laurenyockey@gmail.com>
2016-11-28 12:46:23 -0600

    initialize Slim Menu with custom settings

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
 
     $.backstretch('img/banner.png');
+
+    $('#menu').slimmenu(
+        {
+            resizeWidth: '1015',
+            collapserTitle: '',
+            animSpeed: 'medium',
+            easingEffect: null,
+            indentChildren: false,
+            childrenIndenter: '&nbsp;'
+        });
 });
